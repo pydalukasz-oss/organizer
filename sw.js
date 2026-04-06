4541173a1f38d55e1787e39c8a860cc55cef3d28
pydalukasz-oss <pydalukasz@gmail.com>
2026-04-06 11:44:47 +0200

    Update cache name for service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bizorganizer-v4';
+const CACHE = 'bizorganizer-premium-v1';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
